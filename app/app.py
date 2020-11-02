78b11c983c42099672518e8ecefd9930c801e549
RantoB <bertrandburcker@gmail.com>
2020-11-02 11:31:53 +0100

    update

--- app/app.py
+++ app/app.py
@@ -2,340 +2,93 @@ import dash
 from dash.dependencies import Input, Output
 import dash_core_components as dcc
 import dash_html_components as html
-# import numpy as np
+# from base64 import b64encode
+import cv2
+#import dash_table_experiments as dt
+
+# import datetime
+# import json
 # import pandas as pd
-# import pickle
-# import os
+# import plotly
+# import io
+# import numpy as np
+# from base64 import decodestring
 
 from utils import *
 
-'''Instanciate app'''
-app = dash.Dash(__name__)
+from tensorflow.keras.models import load_model
 
-server = app.server
+app = dash.Dash()
 
-app.title = 'Monuments Strasbourg'
+model_2 = load_model('Models/model_2')
+model_3 = load_model('Models/model_3')
+with open('../data/data-64.pkl', 'rb') as f:
+    data = pickle.load(f)
+target_names = data['target_name_for_answer']
 
-# Messages
-dashboard_name = 'Les monuments de Strasbourg'
-
-description_tab_1 = 'Tab 1.'
-
-description_tab_2 = 'Tab 2.'
-
-description_tab_3 = 'Tab 3.'
-
-description_tab_4 = 'Tab 4'
-
-directory_path = '../'
+# app.scripts.config.serve_locally = True
 
 app.layout = html.Div([
-    html.H4(children=dashboard_name),
-
-    dcc.Tabs([
-        dcc.Tab(label='Label for tab 1', children=[
-            html.H5(children=description_tab_1),
-
-            html.Hr(),
-
-            html.Div([ #
-                html.Div([ # left columns radio items
-                    html.Div(children=[
-                        html.P('selector_1'),
-                        dcc.RadioItems(
-                            id='id_for_selector_1',
-                            options=[
-                                {'label': 'label 1', 'value': '1'},
-                                {'label': 'label 2', 'value': '2'},
-                                {'label': 'label 2', 'value': '3'},
-                                {'label': 'label 3', 'value': '4'},
-                                {'label': 'label 5', 'value': '5'}
-                            ],
-                            value='1',
-                            labelStyle={"display": "block"}
-                        ),
-                    ], style={
-                        'textAlign': 'left'},
-                        className='radio_item'),
-
-                    html.Div(children=[
-                        html.P('selector_2'),
-                        dcc.RadioItems(
-                            id='id_for_selector_2',
-                            options=[
-                                {'label': 'label 1', 'value': '1'},
-                                {'label': 'label 2', 'value': '2'},
-                                {'label': 'label 2', 'value': '3'},
-                                {'label': 'label 3', 'value': '4'},
-                                {'label': 'label 5', 'value': '5'}
-                            ],
-                            value='1',
-                            labelStyle={"display": "block"}
-                        ),
-                    ], style={
-                        'textAlign': 'left'},
-                    className='radio_item'),
-
-                ], className="two columns"),
-
-                html.Div([ # right column - top
-
-
-                    dcc.Upload(
-                        id='upload-image',
-                        children=html.Div([
-                            'Drag and Drop or ',
-                            html.A('Select a picture')
-                        ]),
-                        style={
-                            'width': '100%',
-                            'height': '60px',
-                            'lineHeight': '60px',
-                            'borderWidth': '1px',
-                            'borderStyle': 'dashed',
-                            'borderRadius': '5px',
-                            'textAlign': 'center',
-                            'margin': '10px'
-                        },
-                        # Allow multiple files to be uploaded
-                        multiple=False
-                    ),
-
-                    html.Div(id='output-image-upload'),
-
-                    html.Div(id='output_callback_1', className="row")
-                ], className="ten columns"),
-
-                html.Div([ # right column - bottom
-                    html.Div(id='output_callback_2', className="row")
-                ], className="ten columns")
-
-            ], className="row")],
-            className='custom-tab',
-            selected_className='custom-tab--selected'),
-
-        dcc.Tab(label='Label for tab 2', children=[
-            html.H5(children=description_tab_2),
-                html.Hr(),
-
-                html.Div([ #
-                    html.Div([ # left columns radio items
-                        html.Div(children=[
-                            html.P('Selector 3'),
-                            dcc.RadioItems(
-                                id='id_for_selector_3',
-                                options=[
-                                    {'label': 'label 1', 'value': '1'},
-                                    {'label': 'label 2', 'value': '2'},
-                                    {'label': 'label 2', 'value': '3'},
-                                    {'label': 'label 3', 'value': '4'},
-                                    {'label': 'label 5', 'value': '5'}
-                                ],
-                                value='1',
-                                labelStyle={"display": "block"}
-                            ),
-                        ], style={
-                            'textAlign': 'left'},
-                            className='radio_item'),
-
-                        html.Div(children=[
-                            html.P('Selector 4'),
-                            dcc.RadioItems(
-                                id='id_for_selector_4',
-                                options=[
-                                    {'label': 'label 1', 'value': '1'},
-                                    {'label': 'label 2', 'value': '2'},
-                                    {'label': 'label 2', 'value': '3'},
-                                    {'label': 'label 3', 'value': '4'},
-                                    {'label': 'label 5', 'value': '5'}
-                                ],
-                                value='1',
-                                labelStyle={"display": "block"}
-                            ),
-                        ], style={
-                            'textAlign': 'left'},
-                        className='radio_item'),
-
-                    ], className="two columns"),
-
-                    html.Div([ # right column - top : map and barplot
-                        html.Div(id='output_callback_3', className="row")
-                    ], className="ten columns"),
-
-                    html.Div([ # right column - bottom : line graph
-                        html.Div(id='output_callback_4', className="row")
-                    ], className="ten columns")
-
-                ], className="row")
-            ],
-            className='custom-tab',
-            selected_className='custom-tab--selected'),
-
-        dcc.Tab(label='Label for tab 3', children=[
-            html.H5(children=description_tab_3),
-            html.Hr(),
-            html.Div([ # Main Div
-                html.Div([ # SARIMAX Div
-                    html.Div([ # Left column Div
-                        html.P("text here"),
-                        dcc.Input(
-                            id="input_1", type="number",
-                            value=7, min=1, max=30),
-
-                        html.P("text again here"),
-                        dcc.Input(
-                            id="input_2", type="number",
-                            value=2, min=1, max=14),
-                    ], className="two columns"), # Left column Div
-
-                    html.Div([ # Right column Div
-                        html.H6("blablabla."),
-                        html.Div(id='output_callback_5', className='row'),
-
-                        html.Br(),
-
-                        html.Div([
-                            html.H6("blablabla."),
-                        ], className='row')
-                    ], className="ten columns") # Right column Div
-                ]), # SARIMAX Div
-
-                html.Div([ # PROPHET Div
-                    html.Div([ # Left column Div
-                        html.Hr(),
-                        html.P("youhou !!"),
-                        dcc.Input(
-                            id="input_3", type="number",
-                            value=24, min=1, max=168)
-                    ], className="two columns"), # Left column Div
-
-                    html.Div([ # Right column Div
-                        html.Hr(),
-                        html.H6("one more !"),
-                        html.Div([
-                            # Reality and model comparison
-                            html.Div(id='output_callback_6', className='row'),
-                        ], className='row')
-                    ], className="ten columns") # Right column Div
-                ]) # PROPHET Div
-            ]) # Main Div
-        ],
-            className='custom-tab',
-            selected_className='custom-tab--selected'),
-
-        dcc.Tab(label='Label for tab 4', children=[
-                    html.H5(children=description_tab_4),
-
-                    html.Hr(),
-
-                    html.Div([ #
-                        html.Div([ # left columns
-
-                        ], className="two columns"),
-
-                        html.Div([ # right column
-                            html.P('text 1'),
-                            html.A('link'),
-                            html.Hr(),
-
-                            html.P('text 2'),
-                            html.A('link'),
-                            html.Hr(),
-
-                            html.P('text 3'),
-                            html.A('link'),
-                            html.Hr(),
-
-                            html.P('text 4'),
-                            html.A('link'),
-                            html.Hr(),
-
-                            html.P('text 5'),
-                            html.A('link'),
-
-                        ], className="ten columns")
-                    ], className="row")
-                ],
-             className='custom-tab',
-             selected_className='custom-tab--selected'),
-
-    ], className='custom-tabs')
+    dcc.Upload(
+        id='upload-image',
+        children=html.Div([
+            'Drag and Drop or ',
+            html.A('Select Files')
+        ]),
+        style={
+            'width': '100%',
+            'height': '60px',
+            'lineHeight': '60px',
+            'borderWidth': '1px',
+            'borderStyle': 'dashed',
+            'borderRadius': '5px',
+            'textAlign': 'center',
+            'margin': '10px'
+        },
+        # Allow multiple files to be uploaded
+        multiple=False
+    ),
+    html.Div(id='output-image-upload'),
 ])
+
+def prediction(image_arr):
+    image_arr = process_images_for_model(image_arr)
+
+    pred_mod_2 = model_2.predict([image_arr])
+    pred_mod_3 = model_3.predict([image_arr])
+    pred_23 = (pred_mod_2 + pred_mod_3) / 2.
+
+    proba_pred_23 = pred_23[0][np.argmax(pred_23)]
+
+    return pred_23, proba_pred_23
+
 
 @app.callback(Output('output-image-upload', 'children'),
               [Input('upload-image', 'contents')])
 def update_output(image):
     if not image:
-        return html.P('this is not a picture')
-    else:
-        return html.Img(src=image)
+        return
 
-# Callback for Tab 1
-@app.callback(
-    dash.dependencies.Output('output_callback_1', 'children'),
-    [dash.dependencies.Input('id_for_selector_1', 'value'),
-    dash.dependencies.Input('id_for_selector_2', 'value')])
-def callback_1(id_for_selector_1, id_for_selector_2):
-    result = f'{id_for_selector_1} and {id_for_selector_2} have been selected.'
-    return result
+    image_arr = convert_input_to_arr(image)
 
-@app.callback(
-    dash.dependencies.Output('output_callback_2', 'children'),
-    [dash.dependencies.Input('id_for_selector_2', 'value')])
-def callback_2(id_for_selector_2):
-    result = f'{id_for_selector_2} have been selected.'
-    return result
+    picture = display_picture(image_arr) # To display the picture
 
-# Callback for Tab 2
-@app.callback(
-    dash.dependencies.Output('output_callback_3', 'children'),
-    [dash.dependencies.Input('id_for_selector_3', 'value'),
-    dash.dependencies.Input('id_for_selector_4', 'value')])
-def callback_3(id_for_selector_3, id_for_selector_4):
-    result = f'{id_for_selector_3} and {id_for_selector_4} have been selected.'
-    return result
+    # Code for model prediction
+    pred, proba_pred = prediction(image_arr)
 
-@app.callback(
-    dash.dependencies.Output('output_callback_4', 'children'),
-    [dash.dependencies.Input('id_for_selector_4', 'value')])
-def callback_4(id_for_selector_4):
-    result = f'{id_for_selector_4} have been selected.'
-    return result
+    # prediction = target_names[np.argmax(model.predict(image_arr))]
 
-# Callback for Tab 3
-@app.callback(
-    Output("output_callback_5", "children"),
-    [Input("input_1", "value"),
-    Input("input_2", "value")],
-)
-def callback_5(input_1, input_2):
-    return f'inputs are: {input_1} and {input_2}'
+    answer = result_for_user(pred, proba_pred, target_names)
 
-@app.callback(
-    Output("output_callback_6", "children"),
-    [Input("input_3", "value")],
-)
-def callback_6(input_3):
-    return f'input is: {input_3}'
+    result = html.Div([
 
-def parse_contents(images, images_name):
-
-    try:
-        if images_name.endswith('.jpeg'):
-            pass
-
-    except Exception as e:
-        print(e)
-        return html.Div([
-            'There was an error processing this file.'
-        ])
-
-    return html.Div([
-        html.H5(images_name),
-
-        html.Hr(),  # horizontal line
-
+        # html.Img(src=app.get_asset_url('image_cv2.jpeg')),
+        html.Img(src=f'data:image/jpeg;base64,{picture}'),
+        # html.Img(src=image),
+        html.P(answer)
     ])
 
+    return result
+
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    app.run_server(debug=False)
