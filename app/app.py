5bd60eeb6037dec078dfd0f5e94a332eaa92210c
RantoB <bertrandburcker@gmail.com>
2020-11-03 01:13:46 +0100

    dockerization of application

--- app/app.py
+++ app/app.py
@@ -2,66 +2,191 @@ import dash
 from dash.dependencies import Input, Output
 import dash_core_components as dcc
 import dash_html_components as html
-# from base64 import b64encode
 import cv2
-#import dash_table_experiments as dt
-
-# import datetime
-# import json
-# import pandas as pd
-# import plotly
-# import io
-# import numpy as np
-# from base64 import decodestring
-
-from utils import *
-
 from tensorflow.keras.models import load_model
+from utils import *
 
 app = dash.Dash()
 
-model_2 = load_model('Models/model_2')
-model_3 = load_model('Models/model_3')
-with open('../data/data-64.pkl', 'rb') as f:
+model_2 = load_model('./Models/model_2')
+model_3 = load_model('./Models/model_3')
+with open('./data/data-64.pkl', 'rb') as f:
     data = pickle.load(f)
 target_names = data['target_name_for_answer']
 
 # app.scripts.config.serve_locally = True
 
+details = list()
+
+details.append(dcc.Markdown(children="Vous pouvez tester l'algorithme en postant une photo d'un de ces lieux. \
+        La photo doit être prise du sol. Le batiment ou la statue doivent être vus de \
+        face et doivent se trouver plus ou moins au centre de l'image."))
+
+details.append(dcc.Markdown(children="N'hésitez pas à tester avec d'autres images pour voir si l'algorithme \
+        remarque bien qu'il ne s'agit d'aucun des monuments qu'il connait."))
+
+details.append(dcc.Markdown(children="Note: c'est un projet en cours de développement. La collecte de données \
+        est en cours et le nombre de lieux identifiables va augmenter."))
+
+
+infos = list()
+
+infos.append(html.P(children=["- L'application est développée en Python.\
+        L'image Docker de l'application est disponible sur Dockerhub: ",
+        html.A(children="rantob/image_reco_stras", href="https://hub.docker.com/r/rantob/image_reco_stras"),
+        ". L'application est déployée sur Cloud Run de Google Cloud Platform."]))
+
+infos.append(html.H6("Autres projets:"))
+
+infos.append(html.P(children=["- Dashboard de production d'électricité en France et modèle de prédiction de la demande d'électricité déployé en prototype sur Heroku: ",
+        html.A(children="electricity demand model", href="https://electricityinfrance.herokuapp.com/")]))
+
+infos.append(html.P(children=["- Jeux de piste géants au centre ville de Strasbourg: ",
+        html.A(children="ENIGMA Strasbourg.", href="https://enigmastrasbourg.com"),
+        " Ainsi que son chatbot développé avec RASA et déployé avec docker-compose \
+        sur Google Compute Engine de Google Cloud Platform."]))
+
+infos.append(html.P(children=["Développé par ", html.A(children="Bertrand BURCKER",
+        href="https://www.linkedin.com/in/bertrand-burcker-a6192655/")]))
+
+def consecutive_elements(elements: list)-> list:
+
+    result = list()
+
+    for element in elements:
+        result.append(element)
+        result.append(html.Br())
+
+    return result
+
+with open('./data/encoded_sample_images', 'rb') as f:
+    encoded_sample_images = pickle.load(f)
+
 app.layout = html.Div([
-    dcc.Upload(
-        id='upload-image',
-        children=html.Div([
-            'Drag and Drop or ',
-            html.A('Select Files')
-        ]),
-        style={
-            'width': '100%',
-            'height': '60px',
-            'lineHeight': '60px',
-            'borderWidth': '1px',
-            'borderStyle': 'dashed',
-            'borderRadius': '5px',
-            'textAlign': 'center',
-            'margin': '10px'
-        },
-        # Allow multiple files to be uploaded
-        multiple=False
-    ),
-    html.Div(id='output-image-upload'),
-])
+    html.Div([
 
-def prediction(image_arr):
+        html.H3("Les monuments de Strasbourg"),
+        html.Br(),
+
+        dcc.Markdown(children="Ceci est un prototye de modèle de deep learning pour \
+                la reconnaissance de monuments de Strasbourg."),
+        html.Br(),
+
+        dcc.Markdown(children="Pour l'instant, seuls les six lieux suivants sont identifiables:"),
+        html.Br(),
+
+        html.Div([ # Line with 3 first images
+            html.Div([
+                html.Img(src=f'data:image/jpeg;base64,{encoded_sample_images[0]}'),
+                dcc.Markdown("Cathédrale Notre Dame de Strasbourg")
+
+            ], className="four columns"),
+
+            html.Div([
+                html.Img(src=f'data:image/jpeg;base64,{encoded_sample_images[1]}'),
+                dcc.Markdown("Lycée International des Pontonniers")
+            ], className="four columns"),
+
+            html.Div([
+                html.Img(src=f'data:image/jpeg;base64,{encoded_sample_images[2]}'),
+                dcc.Markdown("Eglise Saint Thomas")
+            ], className="four columns"),
+        ], className="row"),
+
+        html.Div([ # Line with 3 next images
+            html.Div([
+                html.Img(src=f'data:image/jpeg;base64,{encoded_sample_images[3]}'),
+                dcc.Markdown("Le palais Rohan")
+
+            ], className="four columns"),
+
+            html.Div([
+                html.Img(src=f'data:image/jpeg;base64,{encoded_sample_images[4]}'),
+                dcc.Markdown("L'opéra")
+            ], className="four columns"),
+
+            html.Div([
+                html.Img(src=f'data:image/jpeg;base64,{encoded_sample_images[5]}'),
+                dcc.Markdown("L'Aubette à la place Kléber")
+            ], className="four columns"),
+        ], className="row"),
+
+        html.Div([ # Line with 2 col : some text and the POST image area
+            html.Div([ # Text
+                html.Div(consecutive_elements(details)),
+            ], className="six columns",
+            style={
+                'text-align': 'left'
+            }),
+
+            html.Div([ # Image POST area
+                dcc.Upload(
+                    id='upload-image',
+                    children=html.Div([
+                        'Glissez votre fichier ou ',
+                        html.A('parcourez vos dossiers.')
+                    ]),
+                    style={
+                        'width': '100%',
+                        'height': '120px',
+                        'lineHeight': '60px',
+                        'borderWidth': '1px',
+                        'borderStyle': 'dashed',
+                        'borderRadius': '5px',
+                        'textAlign': 'center',
+                        'margin': '10px'
+                    },
+                    multiple=False # Don't allow multiple files to be uploaded
+                ),
+
+                html.Div(id='output-image-upload'),
+            ], className="six columns"),
+        ], className="row"),
+
+        html.Hr(),
+
+        html.Div([
+            html.Div([ # empty area on the left
+                html.P('')
+            ], className="six columns"),
+
+            html.Div([ # Other projects...
+                html.Div(consecutive_elements(infos)),
+            ], className="six columns",
+            style={
+                'text-align': 'left'
+            })
+
+        ], className='row'),
+
+    ], className="row"),
+], style={
+    'width': '75%',
+    'margin-left':'12.5%',
+    'margin-right':'12.5%'
+         }
+)
+
+def prediction(image_arr: np.ndarray) -> tuple:
+    """
+    This function takes as input the image posted by the user
+    and processed by utils.convert_input_to_arr() fonction
+    Returns the array of probabilities
+    and the max probability value.
+    -> (array_of_probabilities, max_proba_value)
+    """
+    # user picture processing
     image_arr = process_images_for_model(image_arr)
-
+    # model_2 prodictions
     pred_mod_2 = model_2.predict([image_arr])
+    # model_3 prodictions
     pred_mod_3 = model_3.predict([image_arr])
+    # Combination predictions of models 2 and 3
     pred_23 = (pred_mod_2 + pred_mod_3) / 2.
-
+    # max probability
+    # [0] is because proba_pred_23.shape is (1, 7)
     proba_pred_23 = pred_23[0][np.argmax(pred_23)]
-
     return pred_23, proba_pred_23
-
 
 @app.callback(Output('output-image-upload', 'children'),
               [Input('upload-image', 'contents')])
@@ -69,26 +194,33 @@ def update_output(image):
     if not image:
         return
 
+    # Convert input to numpy array
     image_arr = convert_input_to_arr(image)
 
-    picture = display_picture(image_arr) # To display the picture
+    # Display the picture
+    saving_path = './tested_pictures/image_cv2.jpeg'
+    picture = display_picture(image_arr, saving_path)
 
-    # Code for model prediction
-    pred, proba_pred = prediction(image_arr)
+    if picture == False:
+        answer = "⚠️ Ce document n'a pas le format d'une image ⚠️"
 
-    # prediction = target_names[np.argmax(model.predict(image_arr))]
+        result = html.Div([
+            html.P(answer)
+        ])
+        return result
 
-    answer = result_for_user(pred, proba_pred, target_names)
+    else:
+        # prediction probabilities
+        pred, proba_pred = prediction(image_arr)
 
-    result = html.Div([
+        # Generate answer string to de displayed to the user
+        answer = result_for_user(pred, proba_pred, target_names)
 
-        # html.Img(src=app.get_asset_url('image_cv2.jpeg')),
-        html.Img(src=f'data:image/jpeg;base64,{picture}'),
-        # html.Img(src=image),
-        html.P(answer)
-    ])
-
-    return result
+        result = html.Div([
+            html.Img(src=f'data:image/jpeg;base64,{picture}'),
+            html.P(answer)
+        ])
+        return result
 
 if __name__ == '__main__':
-    app.run_server(debug=False)
+    app.run_server(host='0.0.0.0', port=8050, debug=False)
--- app/utils.py
+++ app/utils.py
@@ -5,23 +5,38 @@ import pickle
 from base64 import b64encode
 from tensorflow.keras.models import load_model
 
-def convert_input_to_arr(uploaded_image):
+def convert_input_to_arr(uploaded_image: str)-> np.ndarray:
+    """
+    Desciption on going
+    """
     image_bytes = decodebytes(uploaded_image.split(',')[1].encode('ascii'))
     image_array = np.frombuffer(image_bytes, dtype=np.uint8)
     return cv2.imdecode(image_array, flags=cv2.IMREAD_COLOR)
 
-def process_images_for_model(image_arr):
+def process_images_for_model(image_arr: np.ndarray) -> np.ndarray:
+    """
+    Desciption on going
+    """
     image_arr = cv2.resize(image_arr, (64, 64))
     return np.array([image_arr]) / 255.
 
-def display_picture(image_arr):
-    resized_images_arr = cv2.resize(image_arr, (224, int(224 * image_arr.shape[0] / image_arr.shape[1])))
-    images_file_name = 'assets/image_cv2.jpeg'
-    cv2.imwrite(images_file_name, resized_images_arr)
-    encoded_image = b64encode(open(images_file_name, 'rb').read())
+def display_picture(image_arr: np.ndarray, saving_path: str):
+    """
+    Desciption on going
+    """
+    try:
+        resized_images_arr = cv2.resize(image_arr, (224, int(224 * image_arr.shape[0] / image_arr.shape[1])))
+    except:
+        return False
+
+    cv2.imwrite(saving_path, resized_images_arr)
+    encoded_image = b64encode(open(saving_path, 'rb').read())
     return encoded_image.decode()
 
-def result_for_user(pred, proba_pred, target_names):
+def result_for_user(pred, proba_pred, target_names)-> str:
+    """
+    Desciption on going
+    """
 
     if proba_pred >= .85:
         return f'Cette photo montre {target_names[np.argmax(pred)]}'
